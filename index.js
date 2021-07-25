d5984f660668dcf4ef759a6f5b7da312aee867e2
Brendon Mota <bmota@its.jnj.com>
2021-07-25 13:27:02 -0300

    Starting express.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require('express');
+const cors = require('cors');
+const category = require('./src/routes/category');
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(cors());
+
+app.use('/category', category);
+
+app.listen(port);

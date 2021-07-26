5fd65d97a13d46ab1ebe1aa11563ef879a35220f
Brendon Mota <bmota@its.jnj.com>
2021-07-26 13:21:55 -0300

    Search store by naem

---
+++ src/routes/product.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const product = require('../entities/product');
+
+const router = express.Router();
+
+router.get('/:_id/product/:name', (req, res) =>
+  product.search(req.params._id, req.params.name)
+    .then(result => res.send(result))
+    .catch(err => res.status(400).send(err))
+);
+
+router.get('/product/city', (_, res) =>
+  product.getDistinctCity()
+    .then(result => res.send(result))
+    .catch(err => res.status(400).send(err))
+);
+
+
+module.exports = router;

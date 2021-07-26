7ff72a1faefc6a531f4c6316f3ca76deff398ee0
Brendon Mota <bmota@its.jnj.com>
2021-07-26 12:43:18 -0300

    Searching by product name

--- src/entities/product.js
+++ src/entities/product.js
@@ -1,9 +1,20 @@
 const store = require('../models/store');
+const mongoose = require('mongoose');
 
 function getDistinctCity() {
   return store.find().distinct('city');
 }
 
+function search(_id, name) {
+  return store.aggregate([
+    { $match: { _id: mongoose.Types.ObjectId(_id) } },
+    { $unwind: '$products' },
+    { $match: { 'products.name': { $regex: new RegExp(name, 'i') } } },
+    { $group: { _id: '$_id', products: { $push: '$products' } } },
+  ]);
+}
+
 module.exports = {
   getDistinctCity,
+  search,
 };
--- src/routes/store.js
+++ src/routes/store.js
@@ -16,6 +16,12 @@ router.get('/:_id', (req, res) =>
     .catch(err => res.status(400).send(err))
 );
 
+router.get('/:_id/product/:name', (req, res) =>
+  product.search(req.params._id, req.params.name)
+    .then(result => res.send(result))
+    .catch(err => res.status(400).send(err))
+);
+
 router.get('/product/city', (_, res) =>
   product.getDistinctCity()
     .then(result => res.send(result))

da5850f6dcb3a20b833d9238fd145b0670b9a0d8
Brendon Mota <bmota@its.jnj.com>
2021-07-28 08:46:37 -0300

    Starting celebrate

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,25 +1,34 @@
 const express = require('express');
 const store = require('../entities/store');
-const product = require('../entities/product');
+const { celebrate, Joi, errors, Segments } = require('celebrate');
 
 const router = express.Router();
 
-router.get('/store/search', (req, res) =>
-  store.search(req.query)
-    .then(result => res.send(result))
-    .catch(err => res.status(400).send(err))
+router.get('/store/search',
+  celebrate({
+    [Segments.QUERY]: Joi.object({
+      name: Joi.string(),
+      city: Joi.string(),
+    }),
+  }),
+  (req, res) =>
+    store.search(req.query)
+      .then(result => res.send(result))
+      .catch(err => res.status(400).send(err))
 );
 
-router.get('/store/:_id/product/search', (req, res) =>
-  product.getByName(req.params._id, req.query.name)
-    .then(result => res.send(result))
-    .catch(err => res.status(400).send(err))
+router.get('/store/:_id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object({
+      _id: Joi.string().required(),
+    }),
+  }),
+  (req, res) =>
+    store.get(req.params._id)
+      .then(result => res.send(result))
+      .catch(err => res.status(400).send(err))
 );
 
-router.get('/store/:_id', (req, res) =>
-  store.get(req.params._id)
-    .then(result => res.send(result))
-    .catch(err => res.status(400).send(err))
-);
+router.use(errors());
 
 module.exports = router;

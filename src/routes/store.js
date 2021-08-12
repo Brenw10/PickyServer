1b03eb144e9b7bd30d035538fd1a0f5596b3f84a
Brendon Mota <bmota@its.jnj.com>
2021-08-12 16:18:32 -0300

    Change route from store to product file

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,64 +1,9 @@
 const express = require('express');
 const store = require('../services/store');
-const product = require('../services/product');
-const auth = require('../middleware/auth');
 const storePermission = require('../middleware/store');
 const { celebrate, Joi, errors, Segments } = require('celebrate');
 
 const router = express.Router();
-
-router.get('/store/:_id/product/search',
-  celebrate({
-    [Segments.PARAMS]: Joi.object({
-      _id: Joi.string().required(),
-    }),
-    [Segments.QUERY]: Joi.object({
-      'products.name': Joi.string().empty(''),
-      'products.quantity': Joi.number().default(0),
-    }),
-  }),
-  (req, res) =>
-    product.searchByStore(req.params._id, req.query)
-      .then(result => res.send(result))
-      .catch(err => res.status(400).send(err))
-);
-
-router.post('/store/:_id/product',
-  celebrate({
-    [Segments.PARAMS]: Joi.object({
-      _id: Joi.string().required(),
-    }),
-    [Segments.BODY]: Joi.object({
-      name: Joi.string().required(),
-      price: Joi.number().required(),
-      image: Joi.string().required(),
-      quantity: Joi.number().required(),
-      category: Joi.string().required(),
-    }),
-  }),
-  auth, storePermission,
-  (req, res) =>
-    product.create(req.params._id, req.body)
-      .then(result => res.send(result))
-      .catch(err => res.status(400).send(err))
-);
-
-router.put('/store/:_id/product/:product',
-  celebrate({
-    [Segments.PARAMS]: Joi.object({
-      _id: Joi.string().required(),
-      product: Joi.string().required(),
-    }),
-    [Segments.BODY]: Joi.object({
-      quantity: Joi.number().required(),
-    }),
-  }),
-  auth, storePermission,
-  (req, res) =>
-    product.update(req.params._id, req.params.product, req.body)
-      .then(result => res.send(result))
-      .catch(err => res.status(400).send(err))
-);
 
 router.get('/store/search',
   celebrate({

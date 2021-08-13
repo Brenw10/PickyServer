b2982edfb8c6b598bcce2d6b999ff32ad877ec70
Brendon Mota <bmota@its.jnj.com>
2021-08-13 09:24:13 -0300

    improving auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,6 @@
-const express = require('express');
 const user = require('../services/user');
 
-const router = express.Router();
-
-router.use(async function (req, res, next) {
+async function authentication(req, res, next) {
   try {
     const currentUser = await user.getByToken(req.headers.authorization);
     if (!currentUser) throw 'Inexistent User';
@@ -12,6 +9,6 @@ router.use(async function (req, res, next) {
   } catch (err) {
     res.status(400).send(err);
   }
-});
+}
 
-module.exports = router;
+module.exports = authentication;

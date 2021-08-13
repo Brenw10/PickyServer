6348644729c441d8084909c968c2a6cb630dab6d
Brendon Mota <bmota@its.jnj.com>
2021-08-13 10:38:50 -0300

    Improving file.js

--- src/services/file.js
+++ src/services/file.js
@@ -1,13 +1,13 @@
 const fs = require('fs').promises;
 const { v4: uuidv4 } = require('uuid');
 
-const folder = {
-  image: 'public/images/',
+const FOLDER = {
+  IMAGE: 'public/images/',
 };
 
 async function saveBase64Image(base64) {
-  const filepath = folder.image + uuidv4() + '.png';
-  await fs.mkdir(folder.image, { recursive: true });
+  const filepath = FOLDER.IMAGE + uuidv4() + '.png';
+  await fs.mkdir(FOLDER.IMAGE, { recursive: true });
   await fs.writeFile(filepath, base64, { encoding: 'base64' });
   return filepath;
 }

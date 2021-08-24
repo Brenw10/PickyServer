f726e065df477c7245f6c0c24d96d71c944326fd
Brendon Mota <bmota@its.jnj.com>
2021-08-24 17:37:37 -0300

    Avoid issues with non values

--- src/middleware/store.js
+++ src/middleware/store.js
@@ -1,7 +1,7 @@
 function isAllowedUserStore(param) {
   return function (req, res, next) {
     try {
-      if (!res.locals.user.store._id.equals(req.params[param])) throw 'User has no permission to Store';
+      if (!res.locals?.user?.store?._id.equals(req.params?.[param])) throw 'User has no permission to Store';
       next();
     } catch (err) {
       res.status(400).send(err);

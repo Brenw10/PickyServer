58a935bbd5f9fa729c968d04402251408411616b
Brendon Mota <bmota@its.jnj.com>
2021-08-25 18:44:35 -0300

    Created way to remove file and ignore error

--- src/services/file.js
+++ src/services/file.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const FOLDER = {
@@ -7,11 +7,20 @@ const FOLDER = {
 
 async function getBase64ImageSaved(base64) {
   const filepath = FOLDER.IMAGE + uuidv4() + '.png';
-  await fs.mkdir(FOLDER.IMAGE, { recursive: true });
-  await fs.writeFile(filepath, base64, { encoding: 'base64' });
+  await fs.promises.mkdir(FOLDER.IMAGE, { recursive: true });
+  await fs.promises.writeFile(filepath, base64, { encoding: 'base64' });
   return filepath;
+}
+
+async function remove(filepath, ignoreError = true) {
+  try {
+    return await fs.promises.unlink(filepath);
+  } catch (err) {
+    if (!ignoreError) throw err;
+  }
 }
 
 module.exports = {
   getBase64ImageSaved,
+  remove,
 };
